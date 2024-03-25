379e2d36102c99e7bcc59d1140cc6011344d1519
Hoàng Nguyễn <162253280+Hoang3103@users.noreply.github.com>
2024-03-25 16:28:13 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+var messages = [
+    "Em chắc chưa ?",
+    "Thật sự là sẽ k đi à ?",
+    "Nghĩ thật kĩ nha",
+    "Sẽ vui lắm á ",
+    "Không được đâu đừng cố",
+    "Suy nghĩ lại đi",
+    "Em nhầm nút đúng k ?",
+    "Nào đừng làm vậy chứ",
+];
+var button = document.getElementById('yes-button');
+var alertBox = document.getElementById("myAlert");
+function moveButton() {
+    var container = document.getElementById('container');
+    var button = document.getElementById('moveButton');
+    var containerRect = container.getBoundingClientRect();
+    var newX = Math.random() * (containerRect.width - button.offsetWidth);
+    var newY = Math.random() * (containerRect.height - button.offsetHeight);
+    button.style.left = newX + 'px';
+    button.style.top = newY + 'px';
+    // Nút chạy quanh trang web
+    var randomIndex = Math.floor(Math.random() * messages.length);
+    var message = messages[randomIndex];
+    // Thay đổi dòng message ngẫu nhiên trong những dòng ở trên
+    var messageElement = document.getElementById('message');
+    messageElement.textContent = message;
+}
+button.addEventListener("click", function () {
+    // Hiển thị thông báo
+    alertBox.style.display = "block";
+});
+function closeAlert() {
+    alertBox.style.display = "none";
+}
